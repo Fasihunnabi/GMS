b130c5aeb7b3cbecabdf15046f39307e2b72e875
Lycore 9 <fasihunnabi5558@outlook.com>
2020-04-20 03:04:59 +0500

    .

--- orders/templatetags/content_tags.py
+++ orders/templatetags/content_tags.py
@@ -1,15 +1,15 @@
 from django import template
 register = template.Library()
+from product.models import *
 
 
-@register.filter(name='get_cart_count')
-def get_cart_count():
-    print("dvcdvverv")
-    print("dvcdvverv")
-    print("dvcdvverv")
-    print("dvcdvverv")
-    print("dvcdvverv")
-    print("dvcdvverv")
-    print("dvcdvverv")
-    print("dvcdvverv")
-    return "fvfdev"
+@register.filter(name='is_employee')
+def is_employee(id):
+
+    print(id)
+    if Employee.objects.filter(emp_User=id)[:1].exists():
+        print("id existed")
+        return False
+    else:
+        # if User.objects.filter(username='admin').exists() or User.objects.filter(username='localgms').exists():
+        return True
