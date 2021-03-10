6389bf137d33127600b53046295f7c022c4cf824
Lycore 9 <fasihunnabi5558@outlook.com>
2021-03-11 01:33:29 +0500

    .Covid Project set

--- superadmin/urls.py
+++ superadmin/urls.py
@@ -23,37 +23,36 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
 
-    url(r'^login/$', views.login, name="login"),
-    url(r'^logout/$', views.logout, name="logout"),
+    # url(r'^login/$', views.login, name="login"),
+    # url(r'^logout/$', views.logout, name="logout"),
 
-    path('signup', views.user_creation),  #After Login yahan redirect krna
-    path('', login_required(views.index, login_url="/login/")),  #After Login yahan redirect krna
-    path('index/<slug:device_id>', login_required(views.index_with_id, login_url="/login/")),  #After Login yahan redirect krna
+    # path('signup', views.user_creation),  #After Login yahan redirect krna
+    path('', views.index1),  #After Login yahan redirect krna
+    path('covid_data_display/', views.datadisplay, name="display_data"),  #After Login yahan redirect krna
+    # path('index/<slug:device_id>', login_required(views.index_with_id, login_url="/login/")),  #After Login yahan redirect krna
 
-    path('employee', login_required(views.view_employee, login_url="/login/")),  # View Orders
-    path('add-employee/', login_required(views.add_employee, login_url="/login/")),
-    path('edit-employee/<slug:id>', login_required(views.edit_employee, login_url="/login/")),
-    path('delete_employee/', login_required(views.delete_employee.as_view()), name="delete_employee"),  # AJAX to Delete Order
-
-    path('add-device/', login_required(views.add_device, login_url="/login/")),  # Add Device
-    path('edit-device/<slug:id>', login_required(views.edit_device, login_url="/login/")),  # Edit Products
-    path('deletedevice/', login_required(views.deletedevice.as_view()), name="deletedevice"),  # AJAX to Delete Product
-    path('view-devices', login_required(views.view_devices, login_url="/login/")),  # View Products
-
-    path('add-sensor/', login_required(views.add_sensor, login_url="/login/")),  # Add Device
-    path('edit-sensor/<slug:id>', login_required(views.edit_sensor, login_url="/login/")),  # Edit Products
-    path('deletesensor/', login_required(views.deletesensor.as_view()), name="deletesensor"),  # AJAX to Delete Product
-    path('view-sensor', login_required(views.view_sensor, login_url="/login/")),  # View Products
-
-    path('view-cases', login_required(views.view_cases, login_url="/login/")),  # View Products
-    path('add-case', login_required(views.add_case, login_url="/login/")),  # Edit Products
-    path('deletecase/', login_required(views.deletecase.as_view()), name="deletecase"),  # AJAX to Delete Product
-    path('edit-case/<slug:id>', login_required(views.edit_case, login_url="/login/")),  # Edit Products
-
-    path('sensor_reading/', login_required(views.sensor_reading.as_view()), name="sensor_reading"),  # AJAX to Delete Product
-
-    path('case_exist/', login_required(views.case_exist.as_view()), name="case_exist"),  # AJAX to Delete Product
-
-
+    # path('employee', login_required(views.view_employee, login_url="/login/")),  # View Orders
+    # path('add-employee/', login_required(views.add_employee, login_url="/login/")),
+    # path('edit-employee/<slug:id>', login_required(views.edit_employee, login_url="/login/")),
+    # path('delete_employee/', login_required(views.delete_employee.as_view()), name="delete_employee"),  # AJAX to Delete Order
+    #
+    # path('add-device/', login_required(views.add_device, login_url="/login/")),  # Add Device
+    # path('edit-device/<slug:id>', login_required(views.edit_device, login_url="/login/")),  # Edit Products
+    # path('deletedevice/', login_required(views.deletedevice.as_view()), name="deletedevice"),  # AJAX to Delete Product
+    # path('view-devices', login_required(views.view_devices, login_url="/login/")),  # View Products
+    #
+    # path('add-sensor/', login_required(views.add_sensor, login_url="/login/")),  # Add Device
+    # path('edit-sensor/<slug:id>', login_required(views.edit_sensor, login_url="/login/")),  # Edit Products
+    # path('deletesensor/', login_required(views.deletesensor.as_view()), name="deletesensor"),  # AJAX to Delete Product
+    # path('view-sensor', login_required(views.view_sensor, login_url="/login/")),  # View Products
+    #
+    # path('view-cases', login_required(views.view_cases, login_url="/login/")),  # View Products
+    # path('add-case', login_required(views.add_case, login_url="/login/")),  # Edit Products
+    # path('deletecase/', login_required(views.deletecase.as_view()), name="deletecase"),  # AJAX to Delete Product
+    # path('edit-case/<slug:id>', login_required(views.edit_case, login_url="/login/")),  # Edit Products
+    #
+    # path('sensor_reading/', login_required(views.sensor_reading.as_view()), name="sensor_reading"),  # AJAX to Delete Product
+    #
+    # path('case_exist/', login_required(views.case_exist.as_view()), name="case_exist"),  # AJAX to Delete Product
 
 ]
--- superadmin/views.py
+++ superadmin/views.py
@@ -61,6 +61,66 @@ def logout(request):
     return HttpResponseRedirect('/login/')
 
 
+def index1(request):
+    import requests
+    import json
+    try:
+        print(request.GET.get('covid_date' or None).split('-'))
+        a = request.GET.get('covid_date' or None).split('-')
+        print(a[0])
+        url = 'https://api.data.gov.hk/v2/filter?q=%7B%22resource%22%3A%22http%3A%2F%2Fwww.chp.gov.hk%2Ffiles%2Fmisc%2Flatest_situation_of_reported_cases_covid_19_eng.csv%22%2C%22section%22%3A1%2C%22format%22%3A%22json%22%2C%22filters%22%3A%5B%5B1%2C%22eq%22%2C%5B%22{}%2F{}%2F{}%22%5D%5D%5D%7D'.format(
+            str(a[2]), str(a[1]), str(a[0]))
+        # headers = {'Accept':'application/json', 'Content-Type':'application/json'}
+
+        r = requests.get(url)
+        # data = str(r.json())
+        # user_list = json.dumps(r)
+        print(r.json())
+    except:
+        pass
+    context = {
+        'd_id': ""
+    }
+    return render(request, "superadmin/index1.html", context)
+
+
+def datadisplay(request):
+    import requests
+    import json
+    print(request.GET.get('covid_date' or None).split('-'))
+    a = request.GET.get('covid_date' or None).split('-')
+    print(a[0])
+    # url = 'https://api.data.gov.hk/v2/filter?q=%7B%22resource%22%3A%22http%3A%2F%2Fwww.chp.gov.hk%2Ffiles%2Fmisc%2Flatest_situation_of_reported_cases_covid_19_eng.csv%22%2C%22section%22%3A1%2C%22format%22%3A%22json%22%2C%22filters%22%3A%5B%5B1%2C%22eq%22%2C%5B%22{}%2F{}%2F{}%22%5D%5D%5D%7D'.format(
+    #     str(a[2]), str(a[1]), str(a[0]))
+    url = 'https://api.data.gov.hk/v2/filter?q=%7B%22resource%22%3A%22http%3A%2F%2Fwww.chp.gov.hk%2Ffiles%2Fmisc%2Fenhanced_sur_covid_19_eng.csv%22%2C%22section%22%3A1%2C%22format%22%3A%22json%22%2C%22filters%22%3A%5B%5B2%2C%22bw%22%2C%5B%22{}%2F{}%2F{}%22%5D%5D%5D%7D'.format(
+        str(a[2]), str(a[1]), str(a[0]))
+    # headers = {'Accept':'application/json', 'Content-Type':'application/json'}
+
+    r = requests.get(url)
+    # data = str(r.json())
+    # user_list = json.dumps
+    list_cases = []
+    for obj in r.json():
+        print(obj['Case no.'])
+        list_cases.append({
+            'Case_no':obj['Case no.'],
+            'Report_date':obj['Report date'],
+            'Date_of_onset':obj['Date of onset'],
+            'Gender':obj['Gender'],
+            'Age':obj['Age'],
+            'Name_of_hospital':obj['Name of hospital admitted'],
+            'H_D_Deceased':obj['Hospitalised/Discharged/Deceased'],
+            'HK_Non_HK_resident':obj['HK/Non-HK resident'],
+            'Case_classification':obj['Case classification*'],
+            'Confirmed_probable':obj['Confirmed/probable'],
+        })
+    print(list_cases)
+    context = {
+        'covid_case_list': list_cases
+    }
+    return render(request, "superadmin/listcovidcases.html", context)
+
+
 def index(request):
     su_message = request.GET.get("message")
     print(su_message)
